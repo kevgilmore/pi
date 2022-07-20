73a9732940b922c6b54dc9dc3a9b9d9e1b082fc1
kevgilmore <10222150+kevgilmore@users.noreply.github.com>
2022-07-20 14:17:31 +0100

    Create auto_open_chrome.sh

---
+++ auto_open_chrome.sh
@@ -0,0 +1,14 @@
+cd .config
+
+sudo mkdir -p lxsession/LXDE-pi
+
+tee -a lxsession/LXDE-pi/autostart << END
+@lxpanel --profile LXDE-pi
+@pcmanfm --desktop --profile LXDE-pi
+#@xscreensaver -no-splash
+point-rpi
+@chromium-browser --start-fullscreen --start-maximized https://teams.microsoft.com/
+END
+
+
+

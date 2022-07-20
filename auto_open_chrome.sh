cdf03e8845db0cdd59e229b361998e779ba8a7c7
kevgilmore <10222150+kevgilmore@users.noreply.github.com>
2022-07-20 16:34:44 +0100

    Update auto_open_chrome.sh

--- auto_open_chrome.sh
+++ auto_open_chrome.sh
@@ -1,10 +1,10 @@
 cd /home/pi/.config
 
-cp /home/pi/chromium_prefs/Preferences /home/pi/.config/chromium/Default
-
 mkdir -p lxsession/LXDE-pi
 
 tee -a lxsession/LXDE-pi/autostart << END
+cp /home/pi/chromium_prefs/Preferences /home/pi/.config/chromium/Default
+
 @lxpanel --profile LXDE-pi
 @pcmanfm --desktop --profile LXDE-pi
 #@xscreensaver -no-splash

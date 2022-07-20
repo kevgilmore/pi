171e11c998fd5b0ffba6227bab2a52d9dabdf3c5
kevgilmore <10222150+kevgilmore@users.noreply.github.com>
2022-07-20 16:12:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ apt update -y
 apt install vim -y
 apt install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-chsh -s $(which zsh)
+x=`echo "exec zsh"; cat .bashrc`
